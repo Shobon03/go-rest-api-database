a20a94cf2bc49919ee437c189ef08b83b1ed62c6
Shobon03 <syobonaction03@gmail.com>
2024-02-25 00:02:41 -0300

    Fix fixed password on helper function

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,7 +3,7 @@ package helpers
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) string {
-	hash, _ := bcrypt.GenerateFromPassword([]byte("123"), 8)
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), 8)
 
 	return string(hash)
 }
